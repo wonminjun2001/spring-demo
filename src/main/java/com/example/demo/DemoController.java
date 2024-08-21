10b5ce330efc9291c23f7c89dab766c7ee3de89e
minjun <minhtuan@gachon.ac.kr>
2024-08-22 02:21:11 +0900

    first commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DemoController {
+    @RequestMapping("home")
+    public String home() {
+        return "home.html";
+    }
+
+    @RequestMapping("profile")
+    public String profile() {
+        return "profile.html";
+    }
+}
